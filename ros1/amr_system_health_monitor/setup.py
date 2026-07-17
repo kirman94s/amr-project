485ef77d535fafb10cb11c1849337bf0d0459bd6
kirman94s <78927686+kirman94s@users.noreply.github.com>
2026-07-17 14:10:08 +0200

    Add Python package setup for health monitor

---
+++ ros1/amr_system_health_monitor/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="amr_system_health_monitor",
+    version="0.1.0",
+    packages=["amr_system_health_monitor"],
+    package_dir={"": "src"},
+)
